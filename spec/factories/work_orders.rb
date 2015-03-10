911985403b96dc0269b029077416907bff37a4fa
Prelang Builder <builder@prelang.com>
2015-03-10 20:18:51 +0000

    Generated model WorkOrder with migration

---
+++ spec/factories/work_orders.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :work_order do
+    wo_number 1
+start_date "2015-03-10 20:18:51"
+comp_date "2015-03-10 20:18:51"
+requestor "MyString"
+priority 1
+repair_facility "MyString"
+repair_type "MyString"
+location "MyString"
+summary "MyText"
+est_completion "2015-03-10 20:18:51"
+percent_complete "9.99"
+user nil
+  end
+
+end
