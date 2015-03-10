9724f97b40e8af589f062046fbf7e41eb2f97f1f
Prelang Builder <builder@prelang.com>
2015-03-10 20:19:00 +0000

    Generated model Equipment with migration

---
+++ app/models/equipment.rb
@@ -0,0 +1,3 @@
+class Equipment < ActiveRecord::Base
+  belongs_to :work_order
+end
---
+++ spec/factories/equipment.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :equipment do
+    equip_id "MyString"
+model "MyString"
+purchase_date "MyString"
+description "MyText"
+work_order nil
+  end
+
+end
